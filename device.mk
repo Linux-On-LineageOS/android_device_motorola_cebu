4255ef28b0bc17af6e88d8292c4b449e347f0a93
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:32:18 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -88,3 +88,4 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/cebu/cebu-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
